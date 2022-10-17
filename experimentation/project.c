c1a186901eea7284644ecca470e6f6ee4905638d
wanlearner <114071282+wanlearner@users.noreply.github.com>
2022-10-17 21:32:20 +0800

    Add files via upload

---
+++ experimentation/project.c
@@ -0,0 +1,18 @@
+#include <REGX52.H>
+typedef unsigned int u16;//÷ÿ∂®“Â
+typedef unsigned char u8;
+sbit BEEP=P2^5;
+void delay_10us(u16 x){
+	while(x--);
+}
+void main(){
+	int i;
+	i=10;
+	while(i--){
+		BEEP=!BEEP;
+		delay_10us(100);
+	}
+	i=0;
+	BEEP=0;
+}
+		
