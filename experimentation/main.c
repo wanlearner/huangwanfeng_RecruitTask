9623fb6707f5678b13eef60a20b23a114f7a45a0
wanlearner <114071282+wanlearner@users.noreply.github.com>
2022-10-17 21:31:18 +0800

    Add files via upload

--- experimentation/main.c
+++ experimentation/main.c
@@ -1,10 +1,40 @@
 #include <REGX52.H>
-typedef unsigned int u16;
-typedef unsigned char u8;
+#include <INTRINS.H>
+void Delay100ms(x)		//@11.0592MHz
+{
+	unsigned char i, j, k;
+
+	_nop_();
+	_nop_();
+	i = 22;
+	j = 3;
+	k = 227;
+	do
+	{
+		do
+		{
+			while (--k);
+		} while (--j);
+	} while (--i);
+}
+
 void main(){
-	u8 gsmg_code[17]={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,
-	                 0x7c,0x39,0x5e,0x79,0x71};
-	 while(1){
-				P0=gsmg_code[01;
-	 }
- }
+	while(1){
+		P2=0xFE;//1111 1110
+		Delay100ms(100);
+	  P2=0xFD;//1111 1101
+		Delay100ms(100);
+	  P2=0xFB;//1111 1011
+		Delay100ms(200);
+		P2=0xF7;//1111 0111
+		Delay100ms(200);
+		P2=0xEF;//1110 1111
+		Delay100ms(300);
+		P2=0xDF;//1101 1111
+		Delay100ms(400);
+		P2=0xBF;//1011 1111
+		Delay100ms(500);
+		P2=0x7F;//0111 1111
+		Delay100ms(500);
+	}
+	}
