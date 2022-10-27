7d6e61658b4818d223280ce4a0916cc9f1ec89cb
wanlearner <114071282+wanlearner@users.noreply.github.com>
2022-10-27 21:31:27 +0800

    Add files via upload

--- experimentation/main.c
+++ experimentation/main.c
@@ -1,28 +1,16 @@
 #include <REGX52.H>
-typedef unsigned int u8;
-typedef unsigned char u16;
-sbit SRCLK=P3^6;
-sbit PCLK=P3^5;
-sbit SER=P3^4;
-void delay(u8 t){
-  while(t--);
-}
-void hc595_write_date(u8 dat){
-  u8 i=0;
-	for(i=0;i<=8;i++){
-	  SER=dat>>7;
-		dat<<=1;
-		SRCLK=1;
-		delay(1);
-	}
-	PCLK=0;
-	delay(1);
-	PCLK=1;
+typedef unsigned int u16;
+typedef unsigned char u8;
+sbit DC_Motor=P1^0;
+void delay(u16 ms){
+	u16 i,j;
+	for(i=ms;i>0;i--)
+	for(j=110;j>0;j--);
 }
 void main(){
-  u8 i=0;
-	P0=0x7f;
+	DC_Motor=1;
+	delay(500);
+	DC_Motor=0;
 	while(1){
-	  hc595_write_date(0x80);
 	}
 }
